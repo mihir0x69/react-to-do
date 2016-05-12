47774d5fea682512f81160ab45f5528d7416ac22
Mihir Karandikar <Mihir@Mihir-Mac-mini.local>
2016-05-12 11:25:44 +0530

    playground

---
+++ app/tests/components/TodoSearch.test.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoSearch = require('TodoSearch');
+
+describe('TodoSearch', () => {
+
+  it('Should exist', () => {
+    expect(TodoSearch).toExist();
+  });
+
+  it('Should call onSearch with entered input with text', () => {
+    var searchText = 'Dog';
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch handleSearch={spy} />);
+
+    todoSearch.refs.searchText.value = searchText;
+    TestUtils.Simulate.change(todoSearch.refs.searchText);
+
+    expect(spy).toHaveBeenCalledWith(false, searchText);
+  });
+
+  it('Should call onSearch with proper checked value', () => {
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch handleSearch={spy} />);
+
+    todoSearch.refs.showCompleted.checked = true;
+    TestUtils.Simulate.change(todoSearch.refs.showCompleted);
+
+    expect(spy).toHaveBeenCalledWith(true, '');
+  });
+});
---
+++ playground/spread.js
@@ -0,0 +1,5 @@
+var name = ['a', 'b', 'c'];
+
+name.forEach((name) => {
+  console.log('hi ' + name);
+});
