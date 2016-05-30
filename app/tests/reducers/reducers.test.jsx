653e449a642aaccb9a83cd28da29c1ab10e2a62f
Mihir Karandikar <Mihir@Mihir-Mac-mini.local>
2016-05-30 11:11:54 +0530

    added redux store to app.jsx

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -49,7 +49,7 @@ describe('Reducers', () => {
       ];
       var action = {
         type: 'TOGGLE_TODO',
-        id: 123
+        id: '123'
       };
       var res = reducers.todosReducer(df(todos), df(action));
       expect(res[0].completed).toEqual(false);
